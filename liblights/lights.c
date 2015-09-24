a2dd83a81ea5ba0b0fe63ed3f8883f1fd05b50c6
Silentlys <1561707299@qq.com>
2015-09-24 10:36:51 +0800

    Fix notification leds

--- liblights/lights.c
+++ liblights/lights.c
@@ -511,6 +511,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
     .name = "Lights Module",
-    .author = "The CyanogenMod Project",
+    .author = "The panyoujie",
     .methods = &lights_module_methods,
 };
+
