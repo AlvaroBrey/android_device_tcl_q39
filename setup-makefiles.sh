ad3b78de4f41d4684bbba63a838ac3a8f16e96e8
ontherunvaro <varo@ontherunvaro.com>
2016-02-24 22:52:36 +0100

    Fix setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DEVICE=M3G
-VENDOR=TCL
+DEVICE=q39
+VENDOR=tcl
 
 OUTDIR=vendor/$VENDOR/$DEVICE
 MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
@@ -158,7 +158,7 @@ EOF
 
 LOCAL_PATH := \$(call my-dir)
 
-ifeq (\$(TARGET_DEVICE),tomato)
+ifeq (\$(TARGET_DEVICE),$DEVICE)
 ifeq (\$(QCPATH),)
 
 include \$(CLEAR_VARS)
