7b9e3d2e382b2aaade5975bc53f9dae42729e4ee
ontherunvaro <varo@ontherunvaro.com>
2016-02-29 16:37:21 +0100

    Fix (some) headset buttons
    I still need to test other headsets to fully fix this.
    Some info taken from https://github.com/piccolo-dev/android_device_bq_piccolo/issues/5 , credits to Kra1o5 and cmorlok

--- device.mk
+++ device.mk
@@ -188,7 +188,8 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/Goodix-TS.kl:system/usr/keylayout/Goodix-TS.kl
+    $(LOCAL_PATH)/keylayout/Goodix-TS.kl:system/usr/keylayout/Goodix-TS.kl \
+    $(LOCAL_PATH)/keylayout/msm8939-snd-card-skuk_Button_Jack.kl:system/usr/keylayout/msm8939-snd-card-skuk_Button_Jack.kl
     
 # Keystore
 PRODUCT_PACKAGES += \
