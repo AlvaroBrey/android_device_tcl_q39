61e0246bb99282e8ea64b170af0f166cc428b2e1
ontherunvaro <varo@ontherunvaro.com>
2016-10-06 10:24:16 +0200

    product: Move stlport to its own makefile

--- product/display.mk
+++ product/display.mk
@@ -9,8 +9,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-PRODUCT_PACKAGES += \
-     libstlport 
-
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
