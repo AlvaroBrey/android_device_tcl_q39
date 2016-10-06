086b0b856496eb12399590df29ce09bc04a76faa
ontherunvaro <varo@ontherunvaro.com>
2016-10-06 13:54:14 +0200

    Fix kernel debug parameters

--- product/kernel_debug.mk
+++ product/kernel_debug.mk
@@ -5,7 +5,5 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
  	ro.allow.mock.location=1 \
  	ro.debuggable=1 \
  	persist.service.adb.enable=1 \
- 	security.perf_harden=0
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp,adb
+ 	security.perf_harden=0 \
+	persist.sys.usb.config=mtp
